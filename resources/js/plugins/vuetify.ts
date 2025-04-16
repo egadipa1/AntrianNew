8d43fade7a86d53db794f4f4af793339bbc81ae2
Ega Dipa Nusantara <dipanusantaraega@gmail.com>
2025-04-16 09:41:31 +0700

    Fix : Tabrakan Vuetify

--- resources/js/plugins/vuetify.ts
+++ resources/js/plugins/vuetify.ts
@@ -1,9 +1,8 @@
 import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-import 'vuetify/styles' // <-- penting: ini styling bawaan Vuetify
+import { VSwitch } from 'vuetify/components'
 
 export default createVuetify({
-  components,
-  directives,
+  components: {
+    VSwitch,
+  },
 })
